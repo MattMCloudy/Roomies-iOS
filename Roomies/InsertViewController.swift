06ccfabc0ffc30c72faa6557a6ac6669c912de1b
Matthew McLeod <matthew.mcleod@mavs.uta.edu>
2017-01-27 20:11:22 -0600

    navigation improvements

---
+++ Roomies/InsertViewController.swift
@@ -0,0 +1,15 @@
+//
+//  InsertViewController.swift
+//  Roomies
+//
+//  Created by Matthew Mcleod on 1/27/17.
+//  Copyright © 2017 Mobi. All rights reserved.
+//
+
+import GoogleAPIClient
+import GTMOAuth2
+import UIKit
+
+class InsertViewController: UIViewController {
+    
+}
